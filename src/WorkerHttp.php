e0e89aa5f2a35f868768ae95ff13f3f17b43ee9c
呼吸二氧化碳 <jonwang@queyang.com>
2017-10-18 16:05:33 +0800

    静态文件输出类型完善，修复在某些版本下静态文件输出卡住的问题

--- src/WorkerHttp.php
+++ src/WorkerHttp.php
@@ -78,6 +78,117 @@ class WorkerHttp extends Worker
      */
     const FAVICON_ICO_FILE = 'favicon.ico';
 
+    /**
+     * 静态文件类型
+     *
+     * @var array
+     */
+    protected $assetTypes = [
+        'html'    => 'text/html',
+        'htm'     => 'text/html',
+        'shtml'   => 'text/html',
+        'css'     => 'text/css',
+        'xml'     => 'text/xml',
+        'gif'     => 'image/gif',
+        'jpeg'    => 'image/jpeg',
+        'jpg'     => 'image/jpeg',
+        'js'      => 'application/x-javascript',
+        'atom'    => 'application/atom+xml',
+        'rss'     => 'application/rss+xml',
+        'mml'     => 'text/mathml',
+        'txt'     => 'text/plain',
+        'jad'     => 'text/vnd.sun.j2me.app-descriptor',
+        'wml'     => 'text/vnd.wap.wml',
+        'htc'     => 'text/x-component',
+        'png'     => 'image/png',
+        'tif'     => 'image/tiff',
+        'tiff'    => 'image/tiff',
+        'wbmp'    => 'image/vnd.wap.wbmp',
+        'ico'     => 'image/x-icon',
+        'jng'     => 'image/x-jng',
+        'bmp'     => 'image/x-ms-bmp',
+        'svg'     => 'image/svg+xml',
+        'svgz'    => 'image/svg+xml',
+        'webp'    => 'image/webp',
+        'woff'    => 'application/font-woff',
+        'jar'     => 'application/java-archive',
+        'war'     => 'application/java-archive',
+        'ear'     => 'application/java-archive',
+        'json'    => 'application/json',
+        'hqx'     => 'application/mac-binhex40',
+        'doc'     => 'application/msword',
+        'pdf'     => 'application/pdf',
+        'ps'      => 'application/postscript',
+        'eps'     => 'application/postscript',
+        'ai'      => 'application/postscript',
+        'rtf'     => 'application/rtf',
+        'm3u8'    => 'application/vnd.apple.mpegurl',
+        'xls'     => 'application/vnd.ms-excel',
+        'eot'     => 'application/vnd.ms-fontobject',
+        'ppt'     => 'application/vnd.ms-powerpoint',
+        'wmlc'    => 'application/vnd.wap.wmlc',
+        'kml'     => 'application/vnd.google-earth.kml+xml',
+        'kmz'     => 'application/vnd.google-earth.kmz',
+        '7z'      => 'application/x-7z-compressed',
+        'cco'     => 'application/x-cocoa',
+        'jardiff' => 'application/x-java-archive-diff',
+        'jnlp'    => 'application/x-java-jnlp-file',
+        'run'     => 'application/x-makeself',
+        'pl'      => 'application/x-perl',
+        'pm'      => 'application/x-perl',
+        'prc'     => 'application/x-pilot',
+        'pdb'     => 'application/x-pilot',
+        'rar'     => 'application/x-rar-compressed',
+        'rpm'     => 'application/x-redhat-package-manager',
+        'sea'     => 'application/x-sea',
+        'swf'     => 'application/x-shockwave-flash',
+        'sit'     => 'application/x-stuffit',
+        'tcl'     => 'application/x-tcl',
+        'tk'      => 'application/x-tcl',
+        'der'     => 'application/x-x509-ca-cert',
+        'pem'     => 'application/x-x509-ca-cert',
+        'crt'     => 'application/x-x509-ca-cert',
+        'xpi'     => 'application/x-xpinstall',
+        'xhtml'   => 'application/xhtml+xml',
+        'xspf'    => 'application/xspf+xml',
+        'zip'     => 'application/zip',
+        'bin'     => 'application/octet-stream',
+        'exe'     => 'application/octet-stream',
+        'dll'     => 'application/octet-stream',
+        'deb'     => 'application/octet-stream',
+        'dmg'     => 'application/octet-stream',
+        'iso'     => 'application/octet-stream',
+        'img'     => 'application/octet-stream',
+        'msi'     => 'application/octet-stream',
+        'msp'     => 'application/octet-stream',
+        'msm'     => 'application/octet-stream',
+        'docx'    => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'xlsx'    => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'pptx'    => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'mid'     => 'audio/midi',
+        'midi'    => 'audio/midi',
+        'kar'     => 'audio/midi',
+        'mp3'     => 'audio/mpeg',
+        'ogg'     => 'audio/ogg',
+        'm4a'     => 'audio/x-m4a',
+        'ra'      => 'audio/x-realaudio',
+        '3gpp'    => 'video/3gpp',
+        '3gp'     => 'video/3gpp',
+        'ts'      => 'video/mp2t',
+        'mp4'     => 'video/mp4',
+        'mpeg'    => 'video/mpeg',
+        'mpg'     => 'video/mpeg',
+        'mov'     => 'video/quicktime',
+        'webm'    => 'video/webm',
+        'flv'     => 'video/x-flv',
+        'm4v'     => 'video/x-m4v',
+        'mng'     => 'video/x-mng',
+        'asx'     => 'video/x-ms-asf',
+        'asf'     => 'video/x-ms-asf',
+        'wmv'     => 'video/x-ms-wmv',
+        'avi'     => 'video/x-msvideo',
+    ];
+
     public function __construct($arguments)
     {
         parent::__construct($arguments);
@@ -426,15 +537,21 @@ class WorkerHttp extends Worker
         if (is_file($file))
         {
             # 设置缓存头信息
+            $type = strtolower(substr($uri, $rPos + 1));
             $time = 86400;
+            if (isset($this->assetTypes[$type]))
+            {
+                $response->header('Content-Type', $this->assetTypes[$type]);
+            }
+
             $response->header('Cache-Control', 'max-age='. $time);
-            $response->header('Content-Type' , mime_content_type($file));
             $response->header('Pragma'       , 'cache');
             $response->header('Last-Modified', date('D, d M Y H:i:s \G\M\T', filemtime($file)));
             $response->header('Expires'      , date('D, d M Y H:i:s \G\M\T', time() + $time));
 
             # 直接发送文件
             $response->sendfile($file);
+            @$this->server->close($response->fd);       # 不断开的话浏览器会被卡住
         }
         else
         {
