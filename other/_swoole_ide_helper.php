ffc8a438834f1ab01a64ac4697cf1d8d4954e2d9
呼吸二氧化碳 <jonwang@queyang.com>
2019-07-15 12:36:31 +0800

    add getContext()

--- other/_swoole_ide_helper.php
+++ other/_swoole_ide_helper.php
@@ -2986,6 +2986,15 @@ namespace Swoole
          * @see https://wiki.swoole.com/wiki/page/1036.html
          */
         public static function set($setting){}
+
+        /**
+         * 获取当前协程的上下文对象
+         *
+         * @var int $cid
+         * @return Coroutine\Context
+         * @since v4.3.0
+         */
+        public static function getContext($cid = null) {}
     }
 }
 
@@ -4147,6 +4156,118 @@ namespace Swoole\Coroutine
         public function fetchRow (resource $queryResult, int $row = 0){}
 
         public function metaData(String $tableName){}
+    }
+
+    /**
+     * @since 4.4.0
+     */
+    class Context extends \ArrayObject
+    {
+        const STD_PROP_LIST = 1;
+        const ARRAY_AS_PROPS = 2;
+        /**
+         * @param $input [optional]
+         * @param $flags [optional]
+         * @param $iterator_class [optional]
+         * @return mixed
+         */
+        public function __construct($input=null, $flags=null, $iterator_class=null){}
+        /**
+         * @param $index [required]
+         * @return mixed
+         */
+        public function offsetExists($index){}
+        /**
+         * @param $index [required]
+         * @return mixed
+         */
+        public function offsetGet($index){}
+        /**
+         * @param $index [required]
+         * @param $newval [required]
+         * @return mixed
+         */
+        public function offsetSet($index, $newval){}
+        /**
+         * @param $index [required]
+         * @return mixed
+         */
+        public function offsetUnset($index){}
+        /**
+         * @param $value [required]
+         * @return mixed
+         */
+        public function append($value){}
+        /**
+         * @return mixed
+         */
+        public function getArrayCopy(){}
+        /**
+         * @return mixed
+         */
+        public function count(){}
+        /**
+         * @return mixed
+         */
+        public function getFlags(){}
+        /**
+         * @param $flags [required]
+         * @return mixed
+         */
+        public function setFlags($flags){}
+        /**
+         * @return mixed
+         */
+        public function asort(){}
+        /**
+         * @return mixed
+         */
+        public function ksort(){}
+        /**
+         * @param $cmp_function [required]
+         * @return mixed
+         */
+        public function uasort($cmp_function){}
+        /**
+         * @param $cmp_function [required]
+         * @return mixed
+         */
+        public function uksort($cmp_function){}
+        /**
+         * @return mixed
+         */
+        public function natsort(){}
+        /**
+         * @return mixed
+         */
+        public function natcasesort(){}
+        /**
+         * @param $serialized [required]
+         * @return mixed
+         */
+        public function unserialize($serialized){}
+        /**
+         * @return mixed
+         */
+        public function serialize(){}
+        /**
+         * @return mixed
+         */
+        public function getIterator(){}
+        /**
+         * @param $array [required]
+         * @return mixed
+         */
+        public function exchangeArray($array){}
+        /**
+         * @param $iteratorClass [required]
+         * @return mixed
+         */
+        public function setIteratorClass($iteratorClass){}
+        /**
+         * @return mixed
+         */
+        public function getIteratorClass(){}
     }
 }
 
